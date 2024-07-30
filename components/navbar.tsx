4e4d48940b64cbf53d7a1263a5f38ab95b5693d5
dovy2kas <dovixyder@gmail.com>
2024-07-30 20:43:48 +0300

    Made the navigation bar responsive.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,19 +6,19 @@ const Navbar = () => {
     <div className="flex w-full bg-current-line text-foreground sticky top-0 z-10">
       <nav className="container mx-auto p-4 flex justify-between">
         <a href="#home" className="px-3 py-2 rounded hover:text-pink transition flex flex-row space-x-2 items-center">
-          <FaHouse /> <span>Home</span>
+          <span className="text-2xl md:text-md"><FaHouse /></span> <span className="hidden md:flex">Home</span>
         </a>
         <a href="#about" className="px-3 py-2 rounded hover:text-pink transition flex flex-row space-x-2 items-center">
-          <FaUser /> <span>About</span>
+          <span className="text-2xl md:text-md"><FaUser /></span> <span className="hidden md:flex">About</span>
         </a>
         <a href="#projects" className="px-3 py-2 rounded hover:text-pink transition flex flex-row space-x-2 items-center">
-          <FaBook /> <span>Projects</span>
+          <span className="text-2xl md:text-md"><FaBook /></span> <span className="hidden md:flex">Projects</span>
         </a>
         <a href="#services" className="px-3 py-2 rounded hover:text-pink transition flex flex-row space-x-2 items-center">
-          <FaHandHoldingDollar /> <span>Services</span>
+          <span className="text-2xl md:text-md"><FaHandHoldingDollar /></span> <span className="hidden md:flex">Services</span>
         </a>
         <a href="#contact" className="px-3 py-2 rounded hover:text-pink transition flex flex-row space-x-2 items-center">
-          <FaEnvelope /> <span>Contact</span>
+          <span className="text-2xl md:text-md"><FaEnvelope /></span> <span className="hidden md:flex">Contact</span>
         </a>
       </nav>
     </div>
