65b44ab04d14ee4d5818c1a5c7f67fe674f42e7a
dovy2kas <dovixyder@gmail.com>
2025-04-16 18:52:38 +0000

    Removed a url.

--- App.tsx
+++ App.tsx
@@ -31,7 +31,7 @@ function App() {
         <section id="projects" className="w-full">
           <SectionTitle title="Projects"></SectionTitle>
             <Project title="Transacto" desc="A banking website which allows it's users to send and receive, as well as deposit and withdraw funds. This project was made using flask-socketio, nginx, gunicorn and a MySQL database." image={tThumb} alt="Transacto thumbnail" animateIn="fadeInLeft" animateOut="fadeOutRight" url="https://github.com/dovy2kas/transacto"></Project>
-            <Project title="Gambtopia" desc="A gambling website which has two modes: roulette and crash. While building it I learned how to transfer live data, verify the user actions and prove that bets were fair using cryptography. This project was made using flask-socketio, nginx, gunicorn, MySQL and ReCaptcha." image={gThumb} alt="Gambtopia thumbnail" animateIn="fadeInRight" animateOut="fadeOutLeft" url="https://github.com/dovy2kas/gambtopia"></Project>
+            <Project title="Gambtopia" desc="A gambling website which has two modes: roulette and crash. While building it I learned how to transfer live data, verify the user actions and prove that bets were fair using cryptography. This project was made using flask-socketio, nginx, gunicorn, MySQL and ReCaptcha." image={gThumb} alt="Gambtopia thumbnail" animateIn="fadeInRight" animateOut="fadeOutLeft" url="#"></Project>
             <Project title="WPScraper" desc="A script that finds the wordpress website's version and it's plugins." image={wThumb} alt="WPScraper thumbnail" animateIn="fadeInLeft" animateOut="fadeOutRight" url="https://github.com/dovy2kas/wpscraper"></Project>
 
         </section>
